a73a17527099766a917f7c4b70023e940b1817e2
davinwong <davinwong@outlook.com>
2013-07-28 00:31:15 -0400

    proper response handling non-blocking

--- node/request_handlers.js
+++ node/request_handlers.js
@@ -1,9 +1,20 @@
+var exec = require("child_process").exec;
+
 function start() {
     console.log("Request hanlder 'start' was called.");
+
+    exec("ls -lah", function(error, stdout, stderr) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(stdout);
+        response.end();
+    });
 }
 
 function upload() {
     console.log("Request handler 'upload' was called.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Upload");
+    response.end();
 }
 
 exports.start = start;
